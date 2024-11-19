40db2bbe5801d4931920a2c97e4bb726e623a7cf
Matias Davila <matiasdw8@gmail.com>
2024-11-19 10:03:50 -0300

    Continuamos desde la creacion de tablas en DynamoDB

---
+++ Dynamo.py
@@ -0,0 +1,32 @@
+import json 
+import boto3
+
+#Create Table
+client = boto3.client('dynamobd')
+
+def lambda_handler(event, context):
+    response = client.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'CustomerId',
+                'AttributeType': 'S'|'N'|'B'#Select One
+            },
+            {
+                'AttributeName': 'Product',
+                'AttributeType': 'S'|'N'|'B'#Select one
+            },
+        ],
+        TableName = 'string',
+        KeySchema = [
+            {
+                'AttributeName': 'Product',
+                'KeyType': 'HASH'|'RANGE'#Select one
+            },
+        ],
+        ProvisionedThroughput = {
+            'ReadCapacityUnits' : 1,
+            'WriteCapacityUnits': 1
+        },
+    )
+
+#Add items to the table
--- Ec2.py
+++ Ec2.py
@@ -1,6 +1,30 @@
 import boto3
 
+# Crea un cliente de EC2
 client = boto3.client('ec2')
 
-#Create Ec2
+# Crea una instancia EC2
 def lambda_handler(event, context):
+    response = client.run_instances(
+        ImageId='ami-07c3ee614638f120d',  # ID de la imagen de la instancia
+        InstanceType='t2.micro',  # Tipo de instancia
+        MaxCount=1,  # Número máximo de instancias a lanzar
+        MinCount=1  # Número mínimo de instancias a lanzar
+    )
+    # Resto de tu código aquí
+
+# Inicia una instancia EC2
+def Lambda_handler(event, context):
+    response = client.start_instances(
+        InstanceIds=['string'],  # Reemplaza 'string' con el ID de la instancia que deseas iniciar
+        AdditionalInfo='string',  # Opcional: información adicional
+        DryRun=False  # Opcional: establece en True para verificar permisos sin realizar la acción
+    )
+    # Resto de tu código aquí
+
+# Detiene una instancia EC2
+def Lambda_handler(event, context):
+    response = client.stop_instances(
+        InstanceIds=['InstanceId']  # Reemplaza 'InstanceId' con el ID de la instancia que deseas detener
+    )
+    
